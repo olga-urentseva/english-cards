9b6e871b06aade74641eb900177cd54efb426524
Olga Urentseva <lelyalelya9666@gmail.com>
2021-02-25 22:11:30 +0100

    feat(game page): redirect to main page if person is not authenticated

--- src/components/pages/GamePage/index.jsx
+++ src/components/pages/GamePage/index.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 import Container from "../../atoms/Container";
-import MainContainer from "../../atoms/MainContainer";
+import { useAuthContext } from "../../contexts/AuthContext";
 import GameBlock from "../../organisms/GameBlock";
 import Layout from "../../templates/Layout";
+import MainPage from "../MainPage";
 
 const GamePage = () => {
+  const authContextValue = useAuthContext();
+
+  if (!authContextValue.isAuth) {
+    return <MainPage />;
+  }
   return (
     <Layout>
       <Container>
