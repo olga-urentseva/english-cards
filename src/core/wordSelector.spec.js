d6fa4c004d897a5334999ced1db2885439fa3a2c
Olga Urentseva <lelyalelya9666@gmail.com>
2021-04-26 21:14:35 +0200

    test(word selector): unit test

--- src/core/wordSelector.spec.js
+++ src/core/wordSelector.spec.js
@@ -2,15 +2,18 @@ import Word from "./word";
 import WordSelector from "./wordSelector";
 
 describe("WordSelector", () => {
-  it("gives word based on weights", () => {
-    const words = [
+  let words;
+  beforeAll(() => {
+    words = [
       new Word("hello", "привет"),
       new Word("apple", "яблоко"),
       new Word("map", "карта"),
       new Word("cup", "чашка"),
       new Word("table", "стол"),
     ];
+  });
 
+  it("gives word based on weights", () => {
     const wordSelector = new WordSelector(words, [1, 2, 1, 2, 3]);
 
     // |--|----|--|----|------|
@@ -30,5 +33,30 @@ describe("WordSelector", () => {
     wordSelector.randomFn = () => 0.45;
 
     expect(wordSelector.getWord()).toBe(words[3]);
+  });
+
+  it("create a new instance with default value of weights that equal to 1", () => {
+    const wordSelector = new WordSelector(words);
+
+    expect(wordSelector.weightsOfWords).toEqual([1, 1, 1, 1, 1]);
+  });
+
+  it("increase the weight of word", () => {
+    const wordSelector = new WordSelector(words, [1, 1, 1, 1, 1]);
+    wordSelector.increaseWordWeight(words[1]);
+
+    expect(wordSelector.weightsOfWords).toEqual([1, 2, 1, 1, 1]);
+  });
+
+  it("decrease the weight of word with limit", () => {
+    const wordSelector = new WordSelector(words, [1, 2, 1, 1, 1]);
+    wordSelector.decreaseWordWeight(words[1]);
+
+    expect(wordSelector.weightsOfWords).toEqual([1, 1, 1, 1, 1]);
+
+    // decreasing of the word weight is only possible to 1
+
+    wordSelector.decreaseWordWeight(words[1]);
+    expect(wordSelector.weightsOfWords).toEqual([1, 1, 1, 1, 1]);
   });
 });
