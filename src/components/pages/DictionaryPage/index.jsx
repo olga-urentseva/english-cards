e6b893fbf95bd576d4822d51813dc4e79e3a5a32
Olga Urentseva <lelyalelya9666@gmail.com>
2021-05-16 21:41:51 +0200

    feat(DictionaryPage): search form

--- src/components/pages/DictionaryPage/index.jsx
+++ src/components/pages/DictionaryPage/index.jsx
@@ -2,13 +2,31 @@ import React from "react";
 import Container from "../../atoms/Container";
 import CentralContainer from "../../atoms/CentralContainer";
 import Layout from "../../templates/Layout";
+import Input from "../../atoms/Input";
+import SearchButton from "../../atoms/SearchButton";
+
+import classes from "./style.css";
 
 const LibraryPage = () => {
+  const searchWord = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <Layout>
       <Container>
         <CentralContainer>
-          <h2>Словарь:</h2>
+          <form onSubmit={searchWord} className={classes.SearchForm}>
+            <Input
+              id="dictionary-search"
+              placeholder="Поиск слова"
+              className={classes.DictionaryInputSearchInput}
+            />
+            <SearchButton
+              type="submit"
+              className={classes.DictionaryInputSearchButton}
+            />
+          </form>
         </CentralContainer>
       </Container>
     </Layout>
