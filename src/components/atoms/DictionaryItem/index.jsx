f3bd01eddee9eee24fcbf8e8eba7ea876e4d737d
Olga Urentseva <lelyalelya9666@gmail.com>
2021-05-18 21:32:45 +0200

    fix(DictionaryPage): algorytm of searching word

--- src/components/atoms/DictionaryItem/index.jsx
+++ src/components/atoms/DictionaryItem/index.jsx
@@ -3,14 +3,17 @@ import PropTypes from "prop-types";
 
 import classes from "./style.css";
 
-const DictionaryItem = ({ originalWord, translations, id }) => {
+const DictionaryItem = ({ originalWord, translations }) => {
   return (
-    <div className={classes.DictionaryItem} key={id} id={id}>
+    <div className={classes.DictionaryItem}>
       <h3 className={classes.DictionaryItemOriginalWord}>{originalWord}</h3>
       <div className={classes.DictionaryItemtTranslationsWrapper}>
         {translations.map((wordTranslation) => {
           return (
-            <h3 className={classes.DictionaryItemTranslationWord}>
+            <h3
+              className={classes.DictionaryItemTranslationWord}
+              key={wordTranslation}
+            >
               {wordTranslation}
             </h3>
           );
@@ -23,7 +26,6 @@ const DictionaryItem = ({ originalWord, translations, id }) => {
 DictionaryItem.propTypes = {
   originalWord: PropTypes.string,
   translations: PropTypes.array,
-  key: PropTypes.string,
 };
 
 export default DictionaryItem;
--- src/components/pages/DictionaryPage/index.jsx
+++ src/components/pages/DictionaryPage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import Container from "../../atoms/Container";
 import CentralContainer from "../../atoms/CentralContainer";
 import Layout from "../../templates/Layout";
@@ -13,31 +13,60 @@ import classes from "./style.css";
 const DictionaryPage = () => {
   const [inputValue, setInputValue] = useState("");
 
-  const searchWord = (e) => {
-    e.preventDefault();
-    console.log(inputValue);
-  };
+  // const searchWord = (e) => {
+  //   e.preventDefault();
+  //   console.log(inputValue);
+
+  //   const searchedWord = words.find((word) => {
+  //     return (
+  //       word[0] === inputValue ||
+  //       word[1].find((translation) => translation === inputValue)
+  //     );
+  //   });
+  //   console.log(searchedWord);
+
+  //   if (searchedWord) {
+  //     setIsWordFinded(true);
+  //     setSearchedWord(searchedWord);
+  //   } else {
+  //     setSearchedWord("");
+  //     setIsWordFinded(false);
+  //   }
+  // };
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
-  const dictionaryItems = words.map((word) => {
+  const currentWords = useMemo(() => {
+    if (inputValue === "") {
+      return words;
+    }
+    return words.filter(
+      (word) =>
+        word[0].includes(inputValue) ||
+        word[1].some((translation) => translation.includes(inputValue))
+    );
+  }, [inputValue]);
+
+  const dictionaryItems = currentWords.map((word) => {
     return (
       <DictionaryItem
         originalWord={word[0]}
         translations={word[1]}
-        id={word[0]}
+        key={word[0]}
       />
     );
   });
+
+  // create every render, it's not well
 
   return (
     <Layout>
       <Container>
         <CentralContainer>
           <div className={classes.DictionaryPage}>
-            <form onSubmit={searchWord} className={classes.SearchForm}>
+            <form className={classes.SearchForm}>
               <Input
                 id="dictionary-search"
                 placeholder="Поиск слова"
@@ -47,13 +76,19 @@ const DictionaryPage = () => {
                 value={inputValue}
                 onChange={handleInputChange}
               />
-              <SearchButton
+              {/* <SearchButton
                 type="submit"
                 className={classes.DictionaryInputSearchButton}
-              />
+              /> */}
             </form>
             <div className={classes.DictionaryItemsWrapper}>
-              {dictionaryItems}
+              {currentWords.length > 0 ? (
+                dictionaryItems
+              ) : (
+                <h3 className={classes.WordNotFounded}>
+                  Искомое слово не найдено, но у нас есть другие:
+                </h3>
+              )}
             </div>
           </div>
         </CentralContainer>
