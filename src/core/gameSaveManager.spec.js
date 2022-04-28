41de70dda3a57660ab9b1e1601c49c8b8702027e
Olga Urentseva <lelyalelya9666@gmail.com>
2022-04-28 20:23:29 +0200

    added tests for .load .removeSave method on GameSaveManager class

--- src/core/gameSaveManager.spec.js
+++ src/core/gameSaveManager.spec.js
@@ -4,28 +4,45 @@ import GameSaveManager from "./gameSaveManager";
 import Word from "./word";
 
 describe("GameSaveManager", () => {
-  it("save game", () => {
-    const localStorage = {
-      setItem: jest.fn(),
-      getItem: jest.fn(),
-      removeItem: jest.fn(),
-    };
-    const mockedState = {
-      wordsWeightList: [1],
-      score: 100,
-    };
+  const localStorage = {
+    setItem: jest.fn(),
+    getItem: jest.fn(),
+    removeItem: jest.fn(),
+  };
+  const mockedState = {
+    wordsWeightList: [1],
+    score: 100,
+  };
+  const word = new Word("test", ["тест"]);
+  let dictionary;
+  let game;
+  let gameSaver;
+  beforeEach(() => {
+    dictionary = new Dictionary(localStorage, [word]);
+    game = new Game(mockedState, dictionary);
+    gameSaver = new GameSaveManager(localStorage, dictionary);
+  });
+  describe(".save", () => {
+    it("saves game", () => {
+      gameSaver.save(game);
+      expect(localStorage.setItem.mock.calls[0][0]).toBe("gameState");
+      expect(JSON.parse(localStorage.setItem.mock.calls[0][1])).toEqual(
+        mockedState
+      );
+    });
+  });
 
-    const word = new Word("test", ["тест"]);
-    const dictionary = new Dictionary(localStorage, [word]);
-    const game = new Game(mockedState, dictionary);
-    let gameSaver = new GameSaveManager(localStorage, dictionary);
+  describe(".load", () => {
+    it("loads game", () => {
+      const loadedGame = gameSaver.load();
+      expect(loadedGame).toBeInstanceOf(Game);
+    });
+  });
 
-    gameSaver.save(game);
-
-    expect(localStorage.setItem.mock.calls[0][0]).toBe("gameState");
-
-    expect(JSON.parse(localStorage.setItem.mock.calls[0][1])).toEqual(
-      mockedState
-    );
+  describe(".removeSave", () => {
+    it("removes current game from Storage", () => {
+      gameSaver.removeSave();
+      expect(localStorage.removeItem.mock.calls[0][0]).toBe("gameState");
+    });
   });
 });
