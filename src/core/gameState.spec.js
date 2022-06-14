84b000a97180d53c37a251a74db4d0055fd107ec
Olga Urentseva <lelyalelya9666@gmail.com>
2022-06-15 00:15:34 +0200

    add test for gameState class

---
+++ src/core/gameState.spec.js
@@ -0,0 +1,49 @@
+import GameState from "./GameState";
+import Dictionary from "./dictionary";
+
+describe(".GameState", () => {
+  const mockedScore = 0;
+  const mockedWordsWeightList = [1, 1];
+
+  describe(".toString", () => {
+    it("returns JSON", () => {
+      const gameState = new GameState(mockedScore, mockedWordsWeightList);
+
+      const answer = gameState.toString();
+
+      expect(answer).isPrototypeOf(String);
+      expect(
+        answer.includes(mockedScore + "") &&
+          answer.includes(mockedWordsWeightList + "")
+      );
+    });
+  });
+
+  describe("static fromString", () => {
+    it("returns an instance of GameState", () => {
+      const mockedStr = JSON.stringify({
+        score: mockedScore,
+        wordsWeightList: mockedWordsWeightList,
+      });
+      const mockedWords = ["test", ["тест"], ["apple", ["яблоко"]]];
+
+      const dictionary = new Dictionary(undefined, mockedWords);
+      const answer = GameState.fromString(mockedStr, dictionary);
+
+      expect(answer).toBeInstanceOf(GameState);
+    });
+
+    it("returns an start score and start words weights at the game start", () => {
+      const mockedWords = [
+        ["test", ["тест"]],
+        ["apple", ["яблоко"]],
+      ];
+
+      const dictionary = new Dictionary(undefined, mockedWords);
+      const answer = GameState.fromString("", dictionary);
+
+      expect(answer.score).toBe(0);
+      expect(answer.wordsWeightList).toEqual([1, 1]);
+    });
+  });
+});
