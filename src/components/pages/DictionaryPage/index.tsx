97096670d65e2b37011cf504a015b6020e8b41a1
Olga Urentseva <lelyalelya9666@gmail.com>
2022-07-05 19:08:35 +0200

    fix imports from React

--- src/components/pages/DictionaryPage/index.tsx
+++ src/components/pages/DictionaryPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { Navigate } from "react-router-dom";
 
 import Container from "../../atoms/Container";
