792c8785ee920454acc6592a82c9be07afdeb4d4
Olga Urentseva <57731309+olga-urentseva@users.noreply.github.com>
2022-07-25 19:47:10 +0200

    Revert "fix: redirects on Netlify 2"

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   resolve: {
-    extensions: [".mjs", ".js", ".jsx", ".json", ".ts", ".tsx", "_redirects"],
+    extensions: [".mjs", ".js", ".jsx", ".json", ".ts", ".tsx"],
   },
 
   plugins: [
